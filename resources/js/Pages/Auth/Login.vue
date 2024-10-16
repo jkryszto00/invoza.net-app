1359104e425909ebd12af97f988b73e54a07495c
Jakub Krysztofiński <j.krysztofinski@albedomarketing.pl>
2024-10-17 01:49:19 +0200

    created simple login and register views

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>login</h1>
+        <form>
+            <div class="form-input-section">
+                <label for="email" class="input-label">Address email</label>
+                <input type="text" name="email" id="email" placeholder="Your e-mail address" required>
+            </div>
+            <div class="form-input-section">
+                <label for="password" class="input-label">Password</label>
+                <input type="password" name="password" id="password" placeholder="Youre secret password" required>
+            </div>
+            <div class="form-submit-section">
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
+<style>
+.input-label {
+    display: block;
+}
+
+.form-input-section {
+    margin: 10px 0 10px 0;
+}
+
+.form-submit-section {
+    margin-top: 15px;
+}
+</style>
+<script setup>
+</script>
---
+++ resources/js/Pages/Auth/Register.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>register</h1>
+        <form>
+            <div class="form-input-section">
+                <label for="email" class="input-label">Address email</label>
+                <input type="text" name="email" id="email" placeholder="Your e-mail address" required>
+            </div>
+            <div class="form-input-section">
+                <label for="password" class="input-label">Password</label>
+                <input type="password" name="password" id="password" placeholder="Youre secret password" required>
+            </div>
+            <div class="form-input-section">
+                <label for="confirm_password" class="input-label">Confirm password</label>
+                <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm your secret password" required>
+            </div>
+            <div class="form-submit-section">
+                <button type="submit">Register</button>
+            </div>
+        </form>
+    </div>
+</template>
+<style>
+.input-label {
+    display: block;
+}
+
+.form-input-section {
+    margin: 10px 0 10px 0;
+}
+
+.form-submit-section {
+    margin-top: 15px;
+}
+</style>
+<script setup lang="ts">
+</script>
